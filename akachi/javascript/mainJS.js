a39c6a2173240e175f62eef4f02c8e11ccf74a7e
charle426 <114236505+charle426@users.noreply.github.com>
2023-05-22 16:34:14 +0100

    navBar added

---
+++ akachi/javascript/mainJS.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav')
+
+    if(window.scrollY * document.body.offsetHeight >= 2000){
+        nav.classList.add('navFixed');
+        nav.classList.remove('nav');
+    }else{
+        // nav.classList.remove('nav-top-color');
+        nav.classList.add('nav');
+        nav.classList.remove('navFixed');
+    }
+})
+
+console.log(window.scrollY * document.body.offsetHeight)
