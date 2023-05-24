4d1ed9fdd4e92cf7bc270419a781a4d1b25c8ccb
charle426 <114236505+charle426@users.noreply.github.com>
2023-05-24 14:49:23 +0100

    update

--- akachi/javascript/mainJS.js
+++ akachi/javascript/mainJS.js
@@ -12,3 +12,63 @@ window.addEventListener('scroll', () => {
 })
 
 console.log(window.scrollY * document.body.offsetHeight)
+
+// mobile nav
+// function mobileNav(){
+    
+//     const mobNav = document.getElementById('burger').checked 
+    
+//     let w = Number(window.innerWidth);
+//     if(w <= 700) {
+//         if(mobNav){
+//             document.getElementById('ulNone').style.display = 'flex';
+//             document.getElementById('navDivNone').style.display = 'flex';
+//         }else{
+//             document.getElementById('ulNone').style.display = 'none';
+//             document.getElementById('navDivNone').style.display = 'none';
+//         }
+//     }else if(w <= 500){
+//         if(mobNav){
+//         document.getElementById('ulNone').style.display = 'block';
+//         document.getElementById('navDivNone').style.display = 'block';
+//         }else{
+//             document.getElementById('ulNone').style.display = 'none';
+//         document.getElementById('navDivNone').style.display = 'none';
+//         }
+//     }else{
+//        if(document.getElementById('burger').style.display = 'none' ) 
+//         document.getElementById('ulNone').style.display = 'flex';
+//         document.getElementById('navDivNone').style.display = 'flex';
+//     } 
+
+// }
+
+// let wi = Number(window.innerWidth);
+// if(wi => 1000){
+
+// }
+
+
+
+// console.log(window.innerWidth)
+// function screen_resize() {
+//     var h = parseInt(window.innerHeight);
+//     var w = parseInt(window.innerWidth);
+
+//     if(w <= 500) {
+//         //max-width 500px
+//         // actions here...
+//         return mobileNav();
+//     } 
+//     // else if(w > 500 && w <=850) {
+//     //     //max-width 850px
+//     //     // actions here...
+//     //     orange();
+//     // } else {
+//     //     // 850px and beyond
+//     //     // actions here...
+//     //     green();
+//     // }
+
+// }
+// screen_resize()
