aed9cb36c19c3991ca1b0f077daca4115885a5e1
charle426 <114236505+charle426@users.noreply.github.com>
2023-06-20 22:48:22 +0100

    update upgrade

--- akachi/javascript/mainJS.js
+++ akachi/javascript/mainJS.js
@@ -17,10 +17,95 @@ if(document.readyState){
     }, 2000);
 }
 
+
+
 console.log(window.scrollY * document.body.offsetHeight)
 
+var watchingWindows = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) =>{
+            if(entry.isIntersecting){
+                entry.target.classList.add('show')
+                // entry.target.classList.remove('hidden')
+            }
+        })
+    }
+)
 
-function min(a,b){
-    return Math.min(a,b)
+let slide = document.querySelectorAll('.hidden')
+slide.forEach((el) => watchingWindows.observe(el))
+
+
+    const successCallback = (position) => {
+        console.log(position);
+      };
+      
+      const errorCallback = (error) => {
+        console.log(error);
+        alert('Your Location is needed to give you accurate directions')
+        if(error){
+           navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+        }
+      };
+
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+      };
+      
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+      navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+      
+
+var availableHospitals = ["Living Word Hospital", 
+                "Clehansan Hospital And Maternity",
+                "Kelechi Hospital And Maternity",
+                "Ndubuisi Hospital Limited",
+                "Care Hospital",
+                "Simeone Specialist Hospital",
+                "Doctor Ezuma James Ezuma",
+                "Abia State University Teaching Hospital",
+                "New Era Hospital",
+            ]
+                 
+const resultBox = document.querySelector(".resultBox");
+const inputBox = document.getElementById('searchInput');
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableHospitals.filter((keyword) => {
+          return  keyword.toLowerCase().includes(input.toLowerCase())
+        })
+    }
+    displayHospital(result);
+
+    if(!result.length){
+        resultBox.innerHTML = ' ';
+    }
 }
-console.log(min(4,8))
+
+function displayHospital(result){
+    let content = result.map((list) => {
+        return "<li onclick = selectInput(this)>" + list + "</li>"
+    })
+
+    resultBox.innerHTML = "<ul>" + content.join(' ') + "</ul>"
+}                    
+
+function selectInput(listed){
+    inputBox.value = listed.innerHTML;
+    resultBox.innerHTML = ' '
+}
+
+const searchForm = document.querySelector(".searchForm");
+const hospitalNames = document.querySelectorAll(".hospitalNames")
+
+function searchAction(){
+    let inputText = inputBox.value;
+    let hospitalNameText = hospitalNames.innerText
+    if(inputText === hospitalNameText){
+        searchForm.setAttribute("action", "hospitalNameText.trim('-') + '.html'")
+    }
+}
